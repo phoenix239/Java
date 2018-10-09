61bd3c27a003cefb690d870cab83bd2a6b49b757
Adam <Adam@10.0.0.42>
2018-10-08 17:43:07 -0700

    asStudent

---
+++ asStudent/src/asStudent/Student.java
@@ -0,0 +1,12 @@
+package asStudent;
+
+public class Student {
+	/*Create a class Student that provides the following:
+		Private instance variables for holding student id (int), name (String), exam scores (an int array).
+		A public constructor with parameters for initializing student id (int), name (String) and exam scores. The exam scores are passed as an array of int.
+		A public method for returning the final grade as a String ( “A”, “B” etc).
+		Accessor (getter) methods for getting student id and name.*/
+	
+	
+
+}
