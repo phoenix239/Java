9e0fcfd6807221520271c4d1f2da294c4b588645
Adam Boyd <aboyd08@gmail.com>
2018-10-12 17:24:11 -0700

    WTF

---
+++ asStudentExt/src/asStudentExt/StudentExt.java
@@ -0,0 +1,23 @@
+package asStudentExt;
+
+public class StudentExt extends Student{
+		
+	private String type;
+	
+	public StudentExt(int id, String n, int[] ex, String t) {
+		super(id,n,ex);
+		type = t;
+	}
+	
+	public String findGrade ( ){
+
+	    String grade = super.findGrade();
+
+	    if (type.equalsIgnoreCase ("Credit")){
+	    	if ((grade.equalsIgnoreCase("A")) || (grade.equalsIgnoreCase ("B")) || (grade.equalsIgnoreCase ("C"))) grade = "CR";
+	    	else grade = "NCR";
+	    }
+	    
+	    return grade;
+	}
+}
---
+++ asStudentExt/src/asStudentExt/TestStudentExt.java
@@ -0,0 +1,81 @@
+package asStudentExt;
+
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
+public class TestStudentExt {
+
+	public static void main(String[] args) {
+
+		String in, name, grade, type;
+		int numStudent, id, exams;		
+				
+		in = JOptionPane.showInputDialog("Enter number of students");
+		numStudent = Integer.parseInt(in);
+		
+		StudentExt[] st = new StudentExt[numStudent];
+		
+		for(int i = 0; i < st.length; i++) {
+			in = JOptionPane.showInputDialog("Enter data for student number " + (i+1));
+			StringTokenizer tok = new StringTokenizer(in, ",");
+			
+			id = Integer.parseInt(tok.nextToken().trim());
+			name = tok.nextToken().trim();
+			exams = Integer.parseInt(tok.nextToken().trim());
+			
+			int[] scores = new int[exams];
+			for(int j = 0; j < exams; j++) {
+				scores[j] = Integer.parseInt(tok.nextToken().trim());
+			}
+			
+			type = tok.nextToken().trim();
+			
+			st[i] = new StudentExt(id,name,scores,type);			
+		}
+		
+		String[] out = new String[] {"","","","","","",""};
+		
+		for(int i = 0; i < st.length; i++) {
+			grade = st[i].findGrade();
+			if (grade.equalsIgnoreCase("A")){
+				out[0] += st[i].getId() + " " + st[i].getName() + " (A)\n";
+		      }
+		      else if (grade.equalsIgnoreCase("B")){
+		    	out[1] += st[i].getId() + " " + st[i].getName() + " (B)\n";
+		      }
+		      else if (grade.equalsIgnoreCase("C")){
+		    	out[2] += st[i].getId() + " " + st[i].getName() + " (C)\n";
+		      }
+		      else if (grade.equalsIgnoreCase("D")){
+		    	out[3] += st[i].getId() + " " + st[i].getName() + " (D)\n";
+		      }
+		      else if (grade.equalsIgnoreCase("F")){
+		    	out[4] += st[i].getId() + " " + st[i].getName() + " (F)\n";
+		      }
+		      else if (grade.equalsIgnoreCase("CR")){
+		    	out[5] += st[i].getId() + " " + st[i].getName() + " (CR)\n";
+		      }
+		      else {
+		    	out[6] += st[i].getId() + " " + st[i].getName() + " (NCR)\n";
+		      }
+
+		    }
+		
+			displayResult(out);
+			
+			System.exit(0);
+
+	}	
+	
+	public static void displayResult(String[] s){
+		String outAll = "";
+		for (int i = 0; i < s.length; i++){
+	      outAll = outAll + s[i];
+	    }
+
+	    JOptionPane.showMessageDialog(null, outAll);
+
+	}
+	
+}
