bf431e310153564f4634dbecb1645e20d648afe7
Adam Boyd <aboyd08@gmail.com>
2018-11-30 16:43:58 -0800

    asCalculator

---
+++ asCalculator/src/asCalculator/JFrameExt.java
@@ -0,0 +1,9 @@
+package asCalculator;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JFrameExt extends JFrame implements ActionListener{
+
+}
---
+++ asCalculator/src/asCalculator/TestJFrameExt.java
@@ -0,0 +1,14 @@
+package asCalculator;
+
+import javax.swing.JFrame;
+
+public class TestJFrameExt {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize (600, 200);
+		f.setVisible (true);
+
+	}
+
+}
