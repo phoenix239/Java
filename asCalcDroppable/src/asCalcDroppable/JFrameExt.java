410f50bc339e5ca152e5d3e52c2e59a9841c657d
Adam Boyd <aboyd08@gmail.com>
2019-09-05 16:57:58 -0700

    fancier frame

--- asCalcDroppable/src/asCalcDroppable/JFrameExt.java
+++ asCalcDroppable/src/asCalcDroppable/JFrameExt.java
@@ -34,16 +34,17 @@ public class JFrameExt extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 300);
 		contentPane = new JPanel();
+		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(1, 2, 0, 0));
-		
-		JPanelExt panelExt_0 = new JPanelExt();
-		panelExt_0.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		contentPane.add(panelExt_0);
 		
 		JPanelExt panelExt_1 = new JPanelExt();
 		panelExt_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		contentPane.add(panelExt_1);
+		
+		JPanelExt panelExt_0 = new JPanelExt();
+		panelExt_0.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		contentPane.add(panelExt_0);
 	}
 
 }
