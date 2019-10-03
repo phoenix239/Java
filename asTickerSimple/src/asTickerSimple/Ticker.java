918e3d02ec34f92a105a53d6fd4666526246650e
Adam Boyd <aboyd08@gmail.com>
2019-10-03 16:47:31 -0700

    tickersimple done

--- asTickerSimple/src/asTickerSimple/Ticker.java
+++ asTickerSimple/src/asTickerSimple/Ticker.java
@@ -8,43 +8,43 @@ public class Ticker extends JPanel implements Runnable{
 	
 	private boolean moving = false;
 	private String symbol = "DVC 32.0";
-	private int symbolWidth;
 	private int x = 30;
-	private int y = 100;
+	private int y = 110;
 	
 	public Ticker() {
-		moving = false;
-		Thread t1 = new Thread();
-		t1.run();
+		Thread t = new Thread(this);
+		t.start();
 	}
 	
+	@Override	
 	public void run() {
-		//Code the run method to animate the ticker.
 		while(true){
 			if (moving){
-				//Update stock Ticker’s draw co-ordinates.
-				x--;
-				//Call Ticker’s repaint method.
-				repaint();
+				if(x < this.getWidth()) {
+					x += 5;
 				}
-			//Sleep for a fixed time.
+				else {
+					x = -50;
+				}
+				repaint();
+			}
 			try {
-				Thread.sleep(10);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawString(symbol, x, y);
 	}
 	
 	public void setMoving (boolean newMoving ){moving = newMoving;}
 
 	public boolean isMoving ( ){ return moving;}
 	
-	public void paintComponent (Graphics g){
-		super.paintComponent(g);
-		FontMetrics fm = g.getFontMetrics();
-		symbolWidth = fm.stringWidth(symbol);
-		g.drawString(symbol, x, y);
-	}
+	
 }
