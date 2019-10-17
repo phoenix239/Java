4a5b33e10c162e0e87ef81fee874019d58c496df
Adam Boyd <aboyd08@gmail.com>
2019-10-17 14:05:52 -0700

    TickerNotify Creation

---
+++ asTickerNotify/src/asTickerNotify/TickerNotify.java
@@ -0,0 +1,50 @@
+package asTickerNotify;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class TickerNotify extends JPanel implements Runnable{
+	
+	private boolean moving = false;
+	private String symbol = "DVC 32.0";
+	private int x = 30;
+	private int y = 110;
+	
+	public TickerNotify() {
+		Thread t = new Thread(this);
+		t.start();
+	}
+	
+	@Override	
+	public void run() {
+		while(true){
+			if (moving){
+				if(x < this.getWidth()) {
+					x += 5;
+				}
+				else {
+					x = -50;
+				}
+				repaint();
+			}
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawString(symbol, x, y);
+	}
+	
+	public void setMoving (boolean newMoving ){moving = newMoving;}
+
+	public boolean isMoving ( ){ return moving;}
+	
+	
+}
