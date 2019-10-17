53d2977aaa431d4bda258129a7f2c9064545cb79
Adam Boyd <aboyd08@gmail.com>
2019-10-17 14:15:41 -0700

    TickerNotify Done?

--- asTickerNotify/src/asTickerNotify/TickerNotify.java
+++ asTickerNotify/src/asTickerNotify/TickerNotify.java
@@ -4,47 +4,60 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class TickerNotify extends JPanel implements Runnable{
-	
+public class TickerNotify extends JPanel implements Runnable {
+
 	private boolean moving = false;
 	private String symbol = "DVC 32.0";
 	private int x = 30;
 	private int y = 110;
-	
+
 	public TickerNotify() {
 		Thread t = new Thread(this);
 		t.start();
 	}
-	
-	@Override	
+
+	@Override
 	public void run() {
-		while(true){
-			if (moving){
-				if(x < this.getWidth()) {
-					x += 5;
-				}
-				else {
-					x = -50;
-				}
-				repaint();
-			}
+		while (true) {
+			myRun();
 			try {
-				Thread.sleep(50);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
+	public synchronized void myRun() {
+		if (!moving) {
+			try {
+				wait();
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			if (x < this.getWidth()) {
+				x += 5;
+			} else {
+				x = -50;
+			}
+			this.repaint();
+		}
+	}
+
 	@Override
-	public void paintComponent(Graphics g){
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawString(symbol, x, y);
 	}
-	
-	public void setMoving (boolean newMoving ){moving = newMoving;}
 
-	public boolean isMoving ( ){ return moving;}
-	
-	
+	public synchronized void setMoving(boolean moving) {
+		this.moving = moving;
+		notify();
+	}
+
+	public boolean isMoving() {
+		return moving;
+	}
+
 }
